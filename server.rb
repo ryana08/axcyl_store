cdd9cc5dd6e234ee1c732c7d422ceb30a8b8d30a
ryana08 <ryanalmache@gmail.com>
2025-11-26 16:00:56 -0500

    Hotfix 4

--- server.rb
+++ server.rb
@@ -7,25 +7,21 @@ Stripe.api_key = ENV['STRIPE_SECRET_KEY']
 set :static, true
 set :port, ENV.fetch('PORT', 4242)
 set :bind, '0.0.0.0'
+set :protection, except: [:host_authorization]  # Disable host authorization check
 
 YOUR_DOMAIN = 'http://localhost:5500'
 
-# CORS configuration - must be before routes
-configure do
-  enable :cross_origin
-end
-
+# Add CORS back
 before do
   response.headers['Access-Control-Allow-Origin'] = '*'
   response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
-  response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token'
-  response.headers['Access-Control-Max-Age'] = '1728000'
+  response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type, Accept'
 end
 
 options '*' do
   response.headers['Access-Control-Allow-Origin'] = '*'
   response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
-  response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token'
+  response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type, Accept'
   halt 200
 end
 
